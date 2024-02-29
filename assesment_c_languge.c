fdf8fec67162dceb8ebc58cada06ed7f7378c8e2
srushti_patel <145090407+srushtypatel@users.noreply.github.com>
2024-02-29 11:00:50 +0530

    Update assesment_c_languge.c

--- assesment_c_languge.c
+++ assesment_c_languge.c
@@ -1,50 +1,44 @@
 // Write a program to make multiplication of 2-D Matrix 
 
+//multiplication off metrix-1 and metrix-2
 #include <stdio.h>
-int main()
-{
-	
-	 printf("\n ...............................Multiplication...............................\n\n\n");
-    int i, j, k,a1[i][j], a2[i][j], mul[i][j];
+
+int main() {
+    printf("\n ...............................Multiplication...............................\n\n\n");
     
-     printf("\n ...............................Matrix-1...............................");
-    for (i=0;i<2;i++)
-    {
-        for (j=0;j<2;j++)
-        {
-            printf("\n enter first matrix element:");
-            scanf("%d",&a1[i][j]);
+    int i, j, k;
+    int a1[2][2], a2[2][2], mul[2][2];
+    
+    printf("\n ...............................Matrix-1...............................");
+    for (i = 0; i < 2; i++) {
+        for (j = 0; j < 2; j++) {
+            printf("\n enter first matrix element : ", i, j);
+            scanf("%d", &a1[i][j]);
         }
-        
-     	printf("\n");
+        printf("\n");
     }
+    
     printf("\n ...............................Matrix-2...............................");
-    for (i=0;i<2;i++)
-    {
-        for (j=0;j<2;j++)
-        {
-            printf("\n enter second matrix element:");
-            scanf("%d",&a2[i][j]);
+    for (i = 0; i < 2; i++) {
+        for (j = 0; j < 2; j++) {
+            printf("\n enter second matrix element : ", i, j);
+            scanf("%d", &a2[i][j]);
         }
-     	 printf("\n"); 
-  
+        printf("\n");
     }
-  
+    
     printf("\n ...............................Multiplication-Matrix...............................");
     printf("\n\n");
-    for (i=0; i<2; i++) 
-	{
-        for (j=0;j<2;j++)
-		 {
+    for (i = 0; i < 2; i++) {
+        for (j = 0; j < 2; j++) {
             mul[i][j] = 0;
-            for (k=0;k<2;k++) 
-			{
-                mul[i][j]+=a1[i][k]*a2[k][j];
+            for (k = 0; k < 2; k++) {
+                mul[i][j] += a1[i][k] * a2[k][j];
             }
-            printf(" %d ",mul[i][j]); 
+            printf(" %d ", mul[i][j]);
         }
-        printf("\n"); 
+        printf("\n");
     }
     
-     
+    return 0;
 }
